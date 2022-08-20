c6cd350b74d926cd4fe89ba096cc15258cd12edd
Evander Palacios <biomorgoth.palacios@gmail.com>
2022-08-20 01:22:43 -0400

    Add push_send controller + site logic for calling it. Include private key in DER format

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import flask
 import http
 import os
+import pywebpush
 
 application_server_key = os.environ['APPLICATION_SERVER_KEY']
 
@@ -17,7 +18,7 @@ def index():
     """
     return flask.render_template('index.html', application_server_key=application_server_key)
 
-@app.post("/push_subscribe")
+@app.post("/push/subscribe")
 def push_subscribe():
     """
     Receives a Web Push subscription and stores it.
@@ -26,3 +27,32 @@ def push_subscribe():
     # with its corresponding business associations/relations.
     flask.session['push_subscription'] = flask.request.get_json()
     return flask.make_response('', http.HTTPStatus.NO_CONTENT)
+
+@app.post("/push/send")
+def push_send():
+    """
+    Tries to send a WebPush message and responds 204 or 500 depending on the result.
+    """
+    # Example from: https://pypi.org/project/pywebpush/
+    try:
+        pywebpush.webpush(
+            subscription_info=flask.session['push_subscription'],
+            data='Mary had a little lamb, with a nice mint jelly',
+            vapid_private_key=os.environ['PRIVATE_KEY_DER'],
+            vapid_claims={
+                    'sub': 'mailto:{}'.format(os.environ['WEBPUSH_MAILTO']),
+                }
+        )
+
+        return flask.make_response('', http.HTTPStatus.NO_CONTENT)
+    except pywebpush.WebPushException as ex:
+        print("I'm sorry, Dave, but I can't do that: {}", repr(ex))
+        # Mozilla returns additional information in the body of the response.
+        if ex.response and ex.response.json():
+            extra = ex.response.json()
+            print("Remote service replied with a {}:{}, {}",
+                extra.code,
+                extra.errno,
+                extra.message
+                )
+        return flask.make_response('', http.HTTPStatus.INTERNAL_SERVER_ERROR)
--- app/setup.py
+++ app/setup.py
@@ -13,6 +13,14 @@ def format_string(value):
 vapid = py_vapid.Vapid()
 vapid.generate_keys()
 
+# Given how PEM key is generated, we can make a similar approach to have the same key in DER format
+# See: https://github.com/web-push-libs/vapid/blob/a60bcc820a381d46576ade2a22df5480ab4f154c/python/py_vapid/__init__.py#L203
+private_key_der = vapid.private_key.private_bytes(
+    encoding=serialization.Encoding.DER,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
+)
+
 # Generate the applicationServerKey value to embed in the HTML site
 raw_pub = vapid.public_key.public_bytes(
     serialization.Encoding.X962,
@@ -20,8 +28,9 @@ raw_pub = vapid.public_key.public_bytes(
 )
 
 # Output a .env file to be used on the server
-print("APPLICATION_SERVER_KEY={}\nPRIVATE_KEY={}\nPUBLIC_KEY={}\nSECRET_KEY={}".format(
+print("APPLICATION_SERVER_KEY={}\nPRIVATE_KEY_PEM={}\nPUBLIC_KEY_PEM={}\nPRIVATE_KEY_DER={}\nSECRET_KEY={}".format(
     format_string(py_vapid.b64urlencode(raw_pub)),
     format_string(str(vapid.private_pem(), 'utf-8')),
     format_string(str(vapid.public_pem(), 'utf-8')),
+    format_string(py_vapid.utils.b64urlencode(private_key_der)),
     secrets.token_hex()))
